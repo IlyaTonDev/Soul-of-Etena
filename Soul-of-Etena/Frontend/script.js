8e4ebd225c72e7702a69941e83869f46feac0f46
IlyaTonDev <ilyafeofilov90@gmail.com>
2024-07-29 21:59:52 +0300

    Add files via upload

---
+++ Soul-of-Etena/Frontend/script.js
@@ -0,0 +1,19 @@
+const referralButton = document.querySelector('.referral-button');
+const referralPopup = document.querySelector('.referral-popup');
+const inviteButton = document.querySelector('.invite-button');
+
+referralButton.addEventListener('click', () => {
+  referralPopup.classList.add('show');
+});
+
+inviteButton.addEventListener('click', () => {
+    // Откройте ссылку приглашения в новом окне
+    window.open('https://example.com/referral-link', '_blank');
+  });
+
+document.addEventListener('click', (e) => {
+  if (e.target !== referralPopup && e.target !== referralButton) {
+    referralPopup.classList.remove('show');
+  }
+
+});
